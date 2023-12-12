e712ea8dffc99920899dc83082b8e2a085d44a43
Widya Nuri Lailiya <wnuri583@gmail.com>
2023-12-12 10:33:53 +0700

    create backend

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3001
+const router = require('./routes')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.json({
+        message : 'OK'
+    })
+    res.end('OK')
+})
+
+app.use('/berita', router)
+
+app.use((err, req, res, next) =>{
+    const status = err.statusCode || 500
+    console.error(err.message, err.stack)
+    res.status(status).json({
+        message: err.message
+    })
+})
+
+app.listen(port, '0.0.0.0', ()=> {
+    console.log(`app is running on port ${port}`)
+})
